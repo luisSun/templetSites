0a9b96e40c2dcb2a68553124b2f5c403a7e6e95a
luisSun <luis.sun@gmail.com>
2024-03-02 16:53:25 -0300

    Adicionando aside/tags

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -27,10 +27,23 @@ const pnMiddleware = express.static('F:\\movies\\pn', {
   }
 });
 
+const pnmdMiddleware = express.static('F:\\movies\\pn\\midia', {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.ts')) {
+        res.setHeader('Content-Type', 'video/mp2t');
+    } else if (path.endsWith('.mp4')) {
+        res.setHeader('Content-Type', 'video/mp4');
+    } else if (path.endsWith('.wmv')) {
+        res.setHeader('Content-Type', 'video/x-ms-wmv');
+    }
+  }
+});
+
 
 module.exports = {
   staticMiddleware,
   mediaMiddleware,
-  pnMiddleware
+  pnMiddleware,
+  pnmdMiddleware
   // ... Add other middleware functions as needed ...
 };
--- src/routes/homeRoute.js
+++ src/routes/homeRoute.js
@@ -4,51 +4,43 @@ const executeQuery = require('../db/db');
 
 router.get(['/', '/main', '/home'], async (req, res) => {
   try {
-      const selectedItem = await executeQuery('SELECT * FROM pn');
+    const studios = await executeQuery('SELECT DISTINCT studio FROM pn ORDER BY studio');
 
-      // Limit the title to 35 characters
-      if (selectedItem[0]?.title.length > 35) {
-          selectedItem[0].title = selectedItem[0].title.substring(0, 35) + '...';
-      }
+    const selectedItem = await executeQuery('SELECT * FROM pn ORDER BY id DESC');
 
-      const tagsArray = selectedItem[0]?.tag ? selectedItem[0].tag.split(',') : [];
-      const valor = ['Filmes Uncensured', 'javunc', 'javunc', 'jav'];
+    const tagsArray = selectedItem[0]?.tag ? selectedItem[0].tag.split(',') : [];
+    const valor = ['Filmes Uncensured', 'javunc', 'javunc', 'jav'];
 
-      res.render('main', { selectedItem, tagsArray, valor });
-  } catch (error) {
-      console.error('Erro ao recuperar dados do item', error);
-      res.status(500).send('Erro ao recuperar dados do item');
-  }
+    res.render('main', { selectedItem, tagsArray, valor, studios });
+} catch (error) {
+    console.error('Erro ao recuperar dados do item', error);
+    res.status(500).send('Erro ao recuperar dados do item');
+}
 });
 
 
 router.get('/watch', async (req, res) => {
-  try {
-      const selectedItem = await executeQuery('SELECT * FROM pn');
-
-      const tagsArray = selectedItem[0]?.tag ? selectedItem[0].tag.split(',') : [];
-      const valor = ['Filmes Uncensured', 'javunc', 'javunc', 'jav'];
-
-      res.render('teste', { selectedItem, tagsArray, valor });
-  } catch (error) {
-      console.error('Erro ao recuperar dados do item', error);
-      res.status(500).send('Erro ao recuperar dados do item');
-  }
+  res.redirect('/main'); // Redireciona para a rota /main
 });
+
 router.get('/watch/:id', async (req, res) => {
     try {
       const itemId = req.params.id;
+
+      const studios = await executeQuery('SELECT DISTINCT studio FROM pn ORDER BY studio');
   
       const selectedItem = await executeQuery('SELECT * FROM pn WHERE id = ?', [itemId]);
       if (!selectedItem || selectedItem.length === 0) {
         throw new Error('Nenhum item encontrado para o ID fornecido.');
       }
+
+      
       const tagsArray = selectedItem[0].tags.split(',');
       //[1=Title, 2=link video, 3=link img, 4=botao ir main]
       const valor = ['Filmes Idol', 'idol', 'idol', 'idol'];
-      console.log(selectedItem)
+      console.log(tagsArray)
   
-      res.render('teste', { selectedItem: selectedItem[0], tagsArray, valor});
+      res.render('teste', { selectedItem: selectedItem[0], tagsArray, valor,studios});
     } catch (error) {
       console.error('Erro ao recuperar dados do item', error);
       res.status(500).send('Erro ao recuperar dados do item');
